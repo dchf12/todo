f0ac01d8fd6e7ce4e967bae0071e639fc6e18abb
furuya <furuya003@gmail.com>
2022-03-15 22:34:37 +0900

    refactor: Focus refs with useEffect

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface Task {
   id: string | undefined;
@@ -8,10 +8,20 @@ interface Task {
   deleteTask: (arg0: string | undefined) => void;
   editTask: (arg0: string | undefined, newName: string | undefined) => void;
 }
+function usePrevious(value: any) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Todo(props: Task): JSX.Element {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
+  const editFieldRef = useRef<any>(null);
+  const editButtonRef = useRef<any>(null);
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e: { target: { value: React.SetStateAction<string> } }): void {
     setNewName(e.target.value);
@@ -35,6 +45,7 @@ export default function Todo(props: Task): JSX.Element {
           type="text"
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
@@ -57,6 +68,7 @@ export default function Todo(props: Task): JSX.Element {
           type="checkbox"
           defaultChecked={props.completed}
           onChange={() => props.toggleTaskCompleted(props.id)}
+          ref={editButtonRef}
         />
         <label className="todo-label" htmlFor={props.id}>
           {props.name}
@@ -77,5 +89,13 @@ export default function Todo(props: Task): JSX.Element {
     </div>
   );
 
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    }
+    if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
