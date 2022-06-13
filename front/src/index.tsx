18659346235c89a6b70a4d83837ab9a06e417dbf
dchf <dchf.jp@gmail.com>
2022-06-13 20:52:53 +0900

    fix: Change App args to not props

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,20 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-interface Data {
-  id: string | undefined;
-  completed: boolean | undefined;
-  name: {} | null | undefined;
-}
-const DATA: Data[] = [
-  { id: 'todo-0', name: 'Eat', completed: true },
-  { id: 'todo-1', name: 'Sleep', completed: false },
-  { id: 'todo-2', name: 'Repeat', completed: false },
-];
-
 ReactDOM.render(
   <React.StrictMode>
-    <App tasks={DATA} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
