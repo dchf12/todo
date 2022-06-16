21ed46dc3c1daed9676e6e003b549271f67dff0f
dchf <dchf.jp@gmail.com>
2022-06-16 22:17:27 +0900

    fix: Fix routing handler

--- back/main.go
+++ back/main.go
@@ -1,29 +1,12 @@
 package main
 
 import (
-	"log"
-
-	"github.com/dchf12/todo/table"
+	"github.com/dchf12/todo/handler"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	_ "github.com/mattn/go-sqlite3"
 )
 
-const fileName = "todo.sqlite3"
-
 func main() {
-	// database connection
-	db, err := table.InitDB(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	log.Println("Connected!")
-
-	// create table if not exists
-	if err := table.Create(db); err != nil {
-		log.Fatal(err)
-	}
 
 	e := echo.New()
 
@@ -36,10 +19,10 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// routing
-	e.GET("/", table.GetTodos)
-	e.POST("/", table.InsertTodo)
-	e.PUT("/", table.UpdateTodo)
-	e.DELETE("/", table.DeleteTodo)
+	e.GET("/", handler.GetTodos)
+	e.POST("/", handler.AddTodo)
+	e.PUT("/", handler.UpdateTodo)
+	e.DELETE("/", handler.DeleteTodo)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
